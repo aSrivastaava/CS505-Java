b75f849f580a2f45c252ca59ab4c43c26684e788
asrivastaava <adityasrivastaava@gmail.com>
2024-10-14 01:11:48 -0400

    week 5 - modified logic to in 'isIsosceles()' function.

--- week 5/Triangle.java
+++ week 5/Triangle.java
@@ -29,8 +29,7 @@ public class Triangle {
 
     boolean isIsosceles() {
 
-        if ((side1 == side2 && side1 != side3) || (side2 == side3 && side1 != side3)
-                || (side3 == side1 && side1 != side2)) {
+        if ((side1 == side2) || (side2 == side3) || (side3 == side1)) {
             return true;
         }
 
